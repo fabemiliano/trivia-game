2d7179ece5eac68f9cf74cf6d0f884770f0cef53
fabemiliano <fab.emiliano@gmail.com>
2020-07-02 18:25:18 -0300

    Updates

---
+++ precache-manifest.6381ba3001409b3e5a411988d080244c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac74b483a42816d0d21017343f410b04",
+    "url": "/trivia-game/index.html"
+  },
+  {
+    "revision": "abfab1200c4aad37724e",
+    "url": "/trivia-game/static/css/main.58d9a40a.chunk.css"
+  },
+  {
+    "revision": "16f6532b77e7a6d77167",
+    "url": "/trivia-game/static/js/2.09b88ead.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/trivia-game/static/js/2.09b88ead.chunk.js.LICENSE"
+  },
+  {
+    "revision": "abfab1200c4aad37724e",
+    "url": "/trivia-game/static/js/main.f6384ceb.chunk.js"
+  },
+  {
+    "revision": "28c450d90070e1cbeaea",
+    "url": "/trivia-game/static/js/runtime-main.34810379.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia-game/static/media/trivia.466d153e.png"
+  }
+]);
